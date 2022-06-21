251c8b90d2232e235f9c5bf4a23f94bc59cd76c0
Hugo <53018953+YannoteX@users.noreply.github.com>
2022-06-21 15:34:12 +0200

    Revert "update homepage"
    This reverts commit 593b377b08d920f22950ef3f46bfd5b44bef28a3.

--- home.js
+++ home.js
@@ -1,36 +1,14 @@
 $(document).ready(function()
 {
+	console.log("test");
+
+	
 
 	$(".Search").keyup(function(){
-		$.get('http://localhost/API/search.php?NAME='+$(this).val()  ,function(data){
-			console.log($(this).val());
+		$.get("http://localhost/API/search.php?tags=%$city",function(data){
 
-			
-			data=data.replace(/\}\,\{/gm,"};{");
-			data=data.replace("[","");
-			data=data.replace("]","");
-
-			var array = data.split(";");
-			var arrayJson = [];  
-			var dict;
-
-			for(let i=0; i<array.length; i++){
-				dict = $.parseJSON(array[i]);
-				arrayJson.push(dict);
-
-			}
-
-
-
-			for(let v=0; v<arrayJson.length;v++){
-				var imageI ='<img src="'+arrayJson.src+'"alt="'+arrayJson.NAME+'"/>';
-				var image = '<div class="image">'+imageI+'</div>';
-				$("#images").append(image);
-
-
-			}
-
-			console.log(arrayJson);
+			var dict= $.parseJSON(data) ;
+			$("#test").attr(src , data.src );
 
 		});
 
