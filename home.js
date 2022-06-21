71e4319018abbac2a7cdc60aa16e46403d389048
Hugo <53018953+YannoteX@users.noreply.github.com>
2022-06-21 15:38:46 +0200

    update homepage

--- home.js
+++ home.js
@@ -1,14 +1,36 @@
 $(document).ready(function()
 {
-	console.log("test");
-
-	
 
 	$(".Search").keyup(function(){
-		$.get("http://localhost/API/search.php?tags=%$city",function(data){
+		$.get('http://localhost/API/search.php?NAME='+$(this).val()  ,function(data){
+			console.log($(this).val());
 
-			var dict= $.parseJSON(data) ;
-			$("#test").attr(src , data.src );
+			
+			data=data.replace(/\}\,\{/gm,"};{");
+			data=data.replace("[","");
+			data=data.replace("]","");
+
+			var array = data.split(";");
+			var arrayJson = [];  
+			var dict;
+
+			for(let i=0; i<array.length; i++){
+				dict = $.parseJSON(array[i]);
+				arrayJson.push(dict);
+
+			}
+
+
+
+			for(let v=0; v<arrayJson.length;v++){
+				var imageI ='<img src="'+arrayJson.src+'"alt="'+arrayJson.NAME+'"/>';
+				var image = '<div class="image">'+imageI+'</div>';
+				$("#images").append(image);
+
+
+			}
+
+			console.log(arrayJson);
 
 		});
 
