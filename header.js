81145ee2c96c7b0695d409418475e1cf082887b8
Hugo <53018953+YannoteX@users.noreply.github.com>
2022-06-22 15:44:34 +0200

    update header to link user to profile

--- header.js
+++ header.js
@@ -24,7 +24,7 @@ function generateIDHeader(json){
     var logo = '<p><img alt=icon-"' + json.NAME + '" src="' + json.icon + '"/></p>';
     var nom = '<p>' + json.NAME + '</p>';
     var deconnexion = '<p id="deconnexion"><a>deconnexion</a></p>'
-    var header ='<div class="header"><a href="http://localhost/" class="logo">Cloud Share</a><div class="header-left"><a href="#contact">Contact</a><a href="#about">About</a></div><div class="header-right">'+nom+logo+deconnexion+'</div></div>';
+    var header ='<div class="header"><a href="http://localhost/" class="logo">Cloud Share</a><div class="header-left"><a href="#contact">Contact</a><a href="#about">About</a></div><div class="header-right"><a href="http://localhost/profile">'+nom+logo+'</a>'+deconnexion+'</div></div>';
     $('#header').html(header);
     $('#deconnexion').click(function () {
         $.post("http://localhost/API/session.php", {}, function (data) {
