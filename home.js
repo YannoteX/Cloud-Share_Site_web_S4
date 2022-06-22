2a42114906baaa80c42bb42797b47acc6a3d4fe6
Mohamed HAMOUTI <99249120+mohamedhamouti@users.noreply.github.com>
2022-06-22 16:14:07 +0200

    ajout tags home page

--- home.js
+++ home.js
@@ -38,6 +38,59 @@ $(document).ready(function()
 
 	});
 
+	$(".button").click(function(){
+		$.get('http://localhost/API/search.php?tags=%$'+$(this).text()  ,function(data){
+			console.log(data);
+
+			$(this).css("background-color","cyan");
+
+
+			$("#images").html("");
+
+			
+			data=data.replace(/\}\,\{/gm,"};{");
+			data=data.replace("[","");
+			data=data.replace("]","");
+
+			var array = data.split(";");
+			var arrayJson = [];  
+			var dict;
+
+			for(let i=0; i<array.length; i++){
+				dict = $.parseJSON(array[i]);
+				arrayJson.push(dict);
+
+			}
+
+
+
+			for(let v=0; v<arrayJson.length;v++){
+				var imageI ='<img src="'+arrayJson[v].src+'"alt="'+arrayJson.name+'"/>';
+				var image = '<div class="image">'+imageI+'</div>';
+				$("#images").append(image);
+
+
+			}
+
+			
+
+		});
+
+		
+
+	});
+
+	$(".image").click(function(){
+		if($(this).hasClass())
+
+
+
+
+
+
+
+	});
+
 
 
 });
